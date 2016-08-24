97076fb2c20a4afafa348713c2f6e8353b9b78d8
Martin Gorner <mgorner@google.com>
2016-08-24 14:20:45 +0200

    more advanced version of tflearn code

--- mnist_tflearn_convolutional.py
+++ mnist_tflearn_convolutional.py
@@ -1,11 +1,17 @@
-#from sklearn import metrics
 from tensorflow.contrib.learn.python.learn.estimators._sklearn import accuracy_score
 from tensorflow.contrib import learn
+from tensorflow.contrib import layers
+#from tensorflow.contrib import metrics
+from tensorflow.contrib import framework
+from tensorflow.contrib.learn import monitors
+from tensorflow.python.platform import tf_logging as logging
 import tensorflow as tf
 import datetime
-import math
+import numpy as np
 
-def learning_rate(i):
+tf.set_random_seed(0)
+
+def learning_rate(lr, i):
     max_learning_rate = tf.constant(0.003, dtype=tf.float32)
     min_learning_rate = tf.constant(0.0001, dtype=tf.float32)
     decay_speed = tf.constant(2000.0, dtype=tf.float32)
@@ -16,23 +22,76 @@ def learning_rate(i):
 
 mnist = learn.datasets.load_dataset('mnist')
 
-### Convolutional network
 
-def conv_model(X, y):
-    X = tf.reshape(X, [-1, 28, 28, 1])
-    with tf.variable_scope('conv_layer1'):
-        h_conv1 = learn.ops.conv2d(X, n_filters=6, filter_shape=[6, 6], bias=True, activation=tf.nn.relu)
-    with tf.variable_scope('conv_layer2'):
-        h_conv2 = learn.ops.conv2d(h_conv1, n_filters=12, filter_shape=[5, 5], bias=True, activation=tf.nn.relu, strides=[1,2,2,1])
-    with tf.variable_scope('conv_layer3'):
-        h_conv3 = learn.ops.conv2d(h_conv2, n_filters=24, filter_shape=[4, 4], bias=True, activation=tf.nn.relu, strides=[1,2,2,1])
-        h_conv3_flat = tf.reshape(h_conv3, [-1, 7 * 7 * 24])
-    h_fc1 = learn.ops.dnn(h_conv3_flat, [200], activation=tf.nn.relu, dropout=0.75)
-    return learn.models.logistic_regression(h_fc1, y)
+### Convolutional network
+def conv_model(X, Y_, mode):
+    XX = tf.reshape(X, [-1, 28, 28, 1])
+    Y1 = layers.conv2d(XX,  num_outputs=6,  kernel_size=[6, 6], normalizer_fn=layers.batch_norm)
+    Y2 = layers.conv2d(Y1, num_outputs=12, kernel_size=[5, 5], stride=2, normalizer_fn=layers.batch_norm)
+    Y3 = layers.conv2d(Y2, num_outputs=24, kernel_size=[4, 4], stride=2, normalizer_fn=layers.batch_norm)
+    Y4 = layers.flatten(Y3)
+    Y5 = layers.relu(Y4, 200, normalizer_fn=layers.batch_norm)
+    #Y6 = layers.dropout(Y5, keep_prob=0.75, is_training=(mode ==learn.ModeKeys.TRAIN))
+    Ylogits = layers.linear(Y5, 10)
+    #prediction = tf.arg_max(tf.nn.softmax(Ylogits), 1)
+    ## dense to one-hot
+    Y__ = tf.one_hot(tf.cast(Y_, tf.int32), 10, dtype=tf.float32)
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(Ylogits, Y__))
+    train_op = layers.optimize_loss(loss, framework.get_global_step(), 0.003, "Adam", learning_rate_decay_fn=learning_rate)
+    #train_op = layers.optimize_loss(loss, framework.get_global_step(), 0.003, "Adam")
+
+    # 10000 iterations with batch normalization on all layers, no dropout : final accuracy 99.42%
+    # 10000 iterations with batch normalization on all layers, dropout on fully connected layer : final accuracy 99.28%
+    return Ylogits, loss, train_op
+
+def conv_model2(X, Y_, mode):
+    XX = tf.reshape(X, [-1, 28, 28, 1])
+    Y1 = layers.conv2d(XX,  num_outputs=6,  kernel_size=[6, 6], normalizer_fn=layers.batch_norm)
+    Y2 = layers.conv2d(Y1, num_outputs=12, kernel_size=[5, 5], stride=2, normalizer_fn=layers.batch_norm)
+    Y3 = layers.conv2d(Y2, num_outputs=24, kernel_size=[4, 4], stride=2, normalizer_fn=layers.batch_norm)
+    Y4 = layers.flatten(Y3)
+    Y5 = layers.relu(Y4, 200, normalizer_fn=layers.batch_norm)
+    Ylogits = layers.linear(Y5, 10)
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(Ylogits, tf.one_hot(tf.cast(Y_, tf.int32), 10, dtype=tf.float32)))
+    train_op = layers.optimize_loss(loss, framework.get_global_step(), 0.003, "Adam", learning_rate_decay_fn=learning_rate)
+    # 10000 iterations with batch normalization on all layers, 4 conv layers, 2 normal layers, no dropout : final accuracy 99.38%
+    # (layer parameters 6x6x8s1, 5x5x16s2, 4x4x24s2, 3x3x32s1, 400, 200)
+    # 10000 iterations with batch normalization on all layers, 3 conv layers, 2 normal layers, no dropout : final accuracy 99.27%
+    # (layer parameters 6x6x8s1, 5x5x16s2, 4x4x32s2, 400, 200)
+    # 10000 iterations with batch normalization on all layers, 3 conv layers, 2 normal layers, no dropout : final accuracy 99.33%
+    # (layer parameters 6x6x6s1, 5x5x12s2, 4x4x24s2, 200, 50)
+    return Ylogits, loss, train_op
+
+def softmax_model(X, Y_, mode):
+    Ylogits = layers.linear(X, 10)
+    #prediction = tf.nn.softmax(Ylogits)
+    Y__ = tf.one_hot(tf.cast(Y_, tf.int32), 10, dtype=tf.float32)
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(Ylogits, Y__))
+    train_op = layers.optimize_loss(loss, framework.get_global_step(), 0.003, "Adam")
+    return Ylogits, loss, train_op
+
+def dense_model(X, Y_, mode):
+    Y1 = layers.fully_connected(X, 200, normalizer_fn=layers.batch_norm)
+    Ylogits = layers.linear(Y1, 10)
+    #prediction = tf.nn.softmax(Ylogits)
+    Y__ = tf.one_hot(tf.cast(Y_, tf.int32), 10, dtype=tf.float32)
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(Ylogits, Y__))
+    train_op = layers.optimize_loss(loss, framework.get_global_step(), 0.003, "Adam")
+    return Ylogits, loss, train_op
+
+logging.set_verbosity(logging.INFO)
 
 # Training and predicting
-classifier = learn.TensorFlowEstimator(model_fn=conv_model, n_classes=10, batch_size=100, steps=10000, learning_rate=learning_rate, verbose=2)
-timestamp = datetime.datetime.now().timestamp()
-classifier.fit(mnist.train.images, mnist.train.labels, logdir="logs/run"+str(timestamp))
+dense_train_labels = mnist.train.labels.astype(np.int64)
+dense_test_labels = mnist.test.labels.astype(np.int64)
+#mon = monitors.ValidationMonitor(mnist.test.images, dense_test_labels, every_n_steps=100)
+classifier = learn.Classifier(model_fn=conv_model, n_classes=10, model_dir="logs/run"+str(datetime.datetime.now().timestamp()))
+classifier.fit(mnist.train.images, dense_train_labels, batch_size=100, steps=10000)
 score = accuracy_score(mnist.test.labels, classifier.predict(mnist.test.images))
 print('Accuracy: {0:f}'.format(score))
+
+## code graveyard
+
+#classifier = learn.TensorFlowEstimator(model_fn=conv_model, n_classes=10, batch_size=100, optimizer="Adam", learning_rate=learning_rate, verbose=2)
+#one_hot_train_labels = learn.datasets.mnist.dense_to_one_hot(mnist.train.labels, 10).astype(np.float32)
+#one_hot_test_labels = learn.datasets.mnist.dense_to_one_hot(mnist.test.labels, 10).astype(np.float32)
