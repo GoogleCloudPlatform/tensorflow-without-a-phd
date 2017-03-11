425eeb3a25a3e8af4545379317302fb0021e7e94
Abin Simon <abinsimon10@gmail.com>
2017-03-11 08:37:43 +0530

    Fix spacing in the network structure comment

--- mnist_2.1_five_layers_relu_lrdecay.py
+++ mnist_2.1_five_layers_relu_lrdecay.py
@@ -21,17 +21,17 @@ tf.set_random_seed(0)
 
 # neural network with 5 layers
 #
-# · · · · · · · · · ·       (input data, flattened pixels)       X [batch, 784]   # 784 = 28*28
-# \x/x\x/x\x/x\x/x\x/    -- fully connected layer (relu)         W1 [784, 200]      B1[200]
-#  · · · · · · · · ·                                             Y1 [batch, 200]
-#   \x/x\x/x\x/x\x/      -- fully connected layer (relu)         W2 [200, 100]      B2[100]
-#    · · · · · · ·                                               Y2 [batch, 100]
-#    \x/x\x/x\x/         -- fully connected layer (relu)         W3 [100, 60]       B3[60]
-#     · · · · ·                                                  Y3 [batch, 60]
-#     \x/x\x/            -- fully connected layer (relu)         W4 [60, 30]        B4[30]
-#      · · ·                                                     Y4 [batch, 30]
-#      \x/               -- fully connected layer (softmax)      W5 [30, 10]        B5[10]
-#       ·                                                        Y5 [batch, 10]
+# · · · · · · · · · ·          (input data, flattened pixels)       X [batch, 784]   # 784 = 28*28
+# \x/x\x/x\x/x\x/x\x/       -- fully connected layer (relu)         W1 [784, 200]      B1[200]
+#  · · · · · · · · ·                                                Y1 [batch, 200]
+#   \x/x\x/x\x/x\x/         -- fully connected layer (relu)         W2 [200, 100]      B2[100]
+#    · · · · · · ·                                                  Y2 [batch, 100]
+#     \x/x\x/x\x/           -- fully connected layer (relu)         W3 [100, 60]       B3[60]
+#      · · · · ·                                                    Y3 [batch, 60]
+#       \x/x\x/             -- fully connected layer (relu)         W4 [60, 30]        B4[30]
+#        · · ·                                                      Y4 [batch, 30]
+#         \x/               -- fully connected layer (softmax)      W5 [30, 10]        B5[10]
+#          ·                                                        Y5 [batch, 10]
 
 # Download images and labels into mnist.test (10K images+labels) and mnist.train (60K images+labels)
 mnist = read_data_sets("data", one_hot=True, reshape=False, validation_size=0)
