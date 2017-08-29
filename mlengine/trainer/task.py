66cee45e966ee05cfb9049f50cc406f8ec120f2c
Martin Görner <martin.gorner@gmail.com>
2017-08-29 14:39:18 -0700

    added hyperparameter tuning experiments

--- mlengine/trainer/task.py
+++ mlengine/trainer/task.py
@@ -55,11 +55,11 @@ def conv_model_loss(Ylogits, Y_, mode):
 
 
 # Model optimiser (only needed in TRAIN mode)
-def conv_model_train_op(loss, mode):
+def conv_model_train_op(loss, mode, params):
     # Compatibility warning: optimize_loss is still in contrib. This will change in Tensorflow 1.4
-    return tf.contrib.layers.optimize_loss(loss, tf.train.get_global_step(), learning_rate=0.003, optimizer="Adam",
+    return tf.contrib.layers.optimize_loss(loss, tf.train.get_global_step(), learning_rate=params['lr0'], optimizer="Adam",
         # to remove learning rate decay, comment the next line
-        learning_rate_decay_fn=lambda lr, step: 0.0001 + tf.train.exponential_decay(lr, step, -1000, math.e)
+        learning_rate_decay_fn=lambda lr, step: params['lr1'] + tf.train.exponential_decay(lr, step, -params['lr2'], math.e)
         ) if mode == tf.estimator.ModeKeys.TRAIN else None
 
 
@@ -71,24 +71,27 @@ def conv_model_eval_metrics(classes, Y_, mode):
 
 
 # Model
-def conv_model(features, labels, mode):
+def conv_model(features, labels, mode, params):
     X = features['image']
     Y_ = labels
+
+    bias_init = tf.constant_initializer(0.1, dtype=tf.float32)
+    weights_init = tf.truncated_normal_initializer(stddev=0.1)
+
     XX = tf.reshape(X, [-1, 28, 28, 1])
-    biasInit = tf.constant_initializer(0.1, dtype=tf.float32)
-    Y1 = tf.layers.conv2d(XX,  filters=6,  kernel_size=[6, 6], padding="same", activation=tf.nn.relu, bias_initializer=biasInit)
-    Y2 = tf.layers.conv2d(Y1, filters=12, kernel_size=[5, 5], padding="same", strides=2, activation=tf.nn.relu, bias_initializer=biasInit)
-    Y3 = tf.layers.conv2d(Y2, filters=24, kernel_size=[4, 4], padding="same", strides=2, activation=tf.nn.relu, bias_initializer=biasInit)
-    Y4 = tf.reshape(Y3, [-1, 24*7*7])
-    Y5 = tf.layers.dense(Y4, 200, activation=tf.nn.relu, bias_initializer=biasInit)
+    Y1 = tf.layers.conv2d(XX,  filters=params['conv1'],  kernel_size=[6, 6], padding="same", activation=tf.nn.relu, bias_initializer=bias_init, kernel_initializer=weights_init)
+    Y2 = tf.layers.conv2d(Y1, filters=params['conv2'], kernel_size=[5, 5], padding="same", strides=2, activation=tf.nn.relu, bias_initializer=bias_init, kernel_initializer=weights_init)
+    Y3 = tf.layers.conv2d(Y2, filters=params['conv3'], kernel_size=[4, 4], padding="same", strides=2, activation=tf.nn.relu, bias_initializer=bias_init, kernel_initializer=weights_init)
+    Y4 = tf.reshape(Y3, [-1, params['conv3']*7*7])
+    Y5 = tf.layers.dense(Y4, 200, activation=tf.nn.relu, bias_initializer=bias_init, kernel_initializer=weights_init)
     # to deactivate dropout on the dense layer, set rate=1. The rate is the % of dropped neurons.
-    Y5d = tf.layers.dropout(Y5, rate=0.3, training=mode==tf.estimator.ModeKeys.TRAIN)
+    Y5d = tf.layers.dropout(Y5, rate=params['dropout'], training=mode==tf.estimator.ModeKeys.TRAIN)
     Ylogits = tf.layers.dense(Y5d, 10)
     predict = tf.nn.softmax(Ylogits)
     classes = tf.cast(tf.argmax(predict, 1), tf.uint8)
 
     loss = conv_model_loss(Ylogits, Y_, mode)
-    train_op = conv_model_train_op(loss, mode)
+    train_op = conv_model_train_op(loss, mode, params)
     eval_metrics = conv_model_eval_metrics(classes, Y_, mode)
 
     return tf.estimator.EstimatorSpec(
@@ -111,12 +114,12 @@ export_strategy = tf.contrib.learn.utils.saved_model_export_utils.make_export_st
 
 
 # The Experiment is an Estimator with data loading functions and other parameters
-def experiment_fn_with_params(output_dir, data_dir, **kwargs):
+def experiment_fn_with_params(output_dir, hparams, data_dir, **kwargs):
     ITERATIONS = 10000
     mnist = mnist_data.read_data_sets(data_dir, reshape=True, one_hot=False, validation_size=0) # loads training and eval data in memory
     # Compatibility warning: Experiment will move out of contrib in 1.4
     return tf.contrib.learn.Experiment(
-    estimator=tf.estimator.Estimator(model_fn=conv_model, model_dir=output_dir, config=training_config),
+    estimator=tf.estimator.Estimator(model_fn=conv_model, model_dir=output_dir, config=training_config, params=hparams),
     train_input_fn=lambda: train_data_input_fn(mnist),
     eval_input_fn=lambda: eval_data_input_fn(mnist),
     train_steps=ITERATIONS,
@@ -133,14 +136,27 @@ def main(argv):
     # an empty arg -- on the command line:
     # gcloud ml-engine jobs submit training jobXXX --job-dir=... --ml-engine-args -- --user-args
     parser.add_argument('--job-dir', default="checkpoints", help='GCS or local path where to store training checkpoints')
+    parser.add_argument('--data-dir', default="data", help='Where training data will be loaded and unzipped')
+    parser.add_argument('--hp-lr0', default=0.005, type=float, help='Hyperparameter: initial (max) learning rate')
+    parser.add_argument('--hp-lr1', default=0.0002, type=float, help='Hyperparameter: target (min) learning rate')
+    parser.add_argument('--hp-lr2', default=2000, type=float, help='Hyperparameter: learning rate decay speed in steps. Learning rate decays by exp(-1) every N steps.')
+    parser.add_argument('--hp-dropout', default=0.3, type=float, help='Hyperparameter: Dropout rate on dense layers.')
+    parser.add_argument('--hp-conv1', default=6, type=int, help='Hyperparameter: Depth of first convolutional layer.')
+    parser.add_argument('--hp-conv2', default=12, type=int, help='Hyperparameter: Depth of second convolutional layer.')
+    parser.add_argument('--hp-conv3', default=24, type=int, help='Hyperparameter: Depth of third convolutional layer.')
     args = parser.parse_args()
     arguments = args.__dict__
-    arguments['data_dir'] = "data" # Hard-coded here: training data will be downloaded to folder 'data'.
+
+    hparams = {k[3:]: v for k, v in arguments.items() if k.startswith('hp_')}
+    otherargs = {k: v for k, v in arguments.items() if not k.startswith('hp_')}
+
+    logging.log(logging.INFO, "Hyperparameters:" + str(sorted(hparams.items())))
+
+    output_dir = otherargs.pop('job_dir')
 
     # learn_runner needs an experiment function with a single parameter: the output directory.
     # Here we pass additional command line arguments through a closure.
-    output_dir = arguments.pop('job_dir')
-    experiment_fn = lambda output_dir: experiment_fn_with_params(output_dir, **arguments)
+    experiment_fn = lambda output_dir: experiment_fn_with_params(output_dir, hparams, **otherargs)
     # Compatibility warning: learn_runner is currently in contrib. It will move in TF 1.2
     tf.contrib.learn.learn_runner.run(experiment_fn, output_dir)
 
