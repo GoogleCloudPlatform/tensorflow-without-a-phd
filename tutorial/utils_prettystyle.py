7d8cc6a62c71671d9458bfd6449a95ae523ea344
Martin Görner <martin.gorner@gmail.com>
2018-03-05 16:06:25 -0800

    added license information

--- tutorial/utils_prettystyle.py
+++ tutorial/utils_prettystyle.py
@@ -1,6 +1,17 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from matplotlib import pyplot as plt
 
-#def set_custom_plot_style():
 plt.rcParams['figure.figsize']=(16.8,6.0)
 plt.rcParams['axes.grid']=True
 plt.rcParams['axes.linewidth']=0
