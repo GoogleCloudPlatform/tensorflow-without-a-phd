659dd20e96e22b4d2b45c4d1eb869005123a2f89
Martin Görner <martin.gorner@gmail.com>
2018-05-04 15:34:35 -0700

    Added description and artwork in README Changes for Python3 compatibility Removed hard dependencies on pinned version of tensorflow anf numpy
    Change-Id: I94c757da70dd168a5dc9c30130de1a98f6d995e3
    Reviewed-on: https://devrel-review.git.corp.google.com/83104
    Reviewed-by: Yu-Han Liu <yuhanliu@google.com>

--- trainer/helpers.py
+++ trainer/helpers.py
@@ -32,7 +32,7 @@ def discount_rewards(r, gamma):
     r = np.array(r)
     discounted_r = np.zeros_like(r)
     running_add = 0
-    for t in reversed(xrange(0, r.size)):
+    for t in reversed(range(0, r.size)):
         if r[t] != 0: running_add = 0 # reset the sum, since this was a game boundary (pong specific!)
         running_add = running_add * gamma + r[t]
         discounted_r[t] = running_add
