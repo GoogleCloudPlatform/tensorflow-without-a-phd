34c555be164f85e9e9162beb14843ca5a34a6fe6
Yu-Han Liu <yuhanliu@google.com>
2018-09-26 08:46:56 -0700

    replace demo-checkpoint after retraining

--- tensorflow-rl-pong/submit.sh
+++ tensorflow-rl-pong/submit.sh
@@ -25,7 +25,7 @@ JOB_NAME="pong_$now"
 JOB_DIR=$BUCKET$JOB_NAME
 
 gcloud ml-engine jobs submit training $JOB_NAME \
-    --job-dir $JOB_DIR  \
+    --job-dir $JOB_DIR \
     --package-path $TRAINER_PACKAGE_PATH \
     --module-name $MAIN_TRAINER_MODULE \
     --region us-central1 \
