bc4da46a1731bb252ee1a7f1b63c36e315eeb134
Martin Görner <martin.gorner@gmail.com>
2018-11-30 15:45:04 -0800

    use-tpu = True on TPU by default
    Change-Id: I5642618bc9909af14cdf4737c763c825b578edfc

--- tensorflow-planespotting/trainer_yolo/main.py
+++ tensorflow-planespotting/trainer_yolo/main.py
@@ -192,7 +192,7 @@ def main(argv):
     parser.add_argument('--hp-data-rnd-orientation', default=True, type=str2bool, help='Data generation hyperparameter: data augmentation by rotating and flipping tiles.')
     parser.add_argument('--hp-data-cache-n-epochs', default=0, type=int, help='Generate random data variations for n epochs then cache and reuse.')
     # switch for training on TPUs
-    parser.add_argument('--hp-use-tpu', default=False, type=str2bool, help='Enable training on TPUs')
+    parser.add_argument('--hp-use-tpu', default=True, type=str2bool, help='Enable training on TPUs')
     parser.add_argument('--hp-tpu-iterations', default=1000, type=int, help='Iterations per call to the TPU')
     # TPUEstimator uses the following parameters internally - do not use them
     parser.add_argument('--tpu', default=None, help='(internal) ML Engine uses this argument to pass the IP address of the TPU')
